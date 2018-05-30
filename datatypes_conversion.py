22c1d46d8a2ede0ec69c45d5ced9090c4531fd43
luckynalamothu <39589219+luckynalamothu@users.noreply.github.com>
2018-05-30 15:35:53 +0530

    Create datatypes_conversion.py

---
+++ datatypes_conversion.py
@@ -0,0 +1,33 @@
+#############################################################################
+####This is python practise programs#########################################
+#
+# Below one explains about the dataypes of int and float
+#
+#   Assgning the values of the int and float
+#   calculating the c_sum value by adding a_int and b_float
+#   printing the c_sum value by using the print function
+#   printing the which type of datatype is obtained finally
+#
+# Conversions of Datatypes from one form to another form
+# 
+#     Assigning the values of the integer type      
+#
+###############$$$$$$$$$$$$$$$$$@@@@@@@@@@@@@@@@@@@@@@@###########$$$$$$$$$
+#Author:Luckynalamothu
+#Date:30-5-2018
+######################################################################
+
+
+a_int = 1 # Assgning value to an integer variable
+
+b_float = 1.0 # Assigning value to an float variable
+c_sum = a_int + b_float # Adding the values of a and b
+print(c_sum) # Printing the sum value
+type(c_sum) # Prints the datatype of the variable (c_cum)
+
+
+a_int = 3 # Assgning value to an integer variable
+b_int = 2 # Assgning value to an integer variable
+c_float_sum = float(a_int + b_int) # Explicit type conversion from int to float
+
+
